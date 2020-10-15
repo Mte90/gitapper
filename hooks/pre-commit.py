6b6aba72d3735dff3c5010aa09d9cdd7e3a20201
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-15 16:30:47 +0200

    feat(commit): improved commit detection

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -4,17 +4,17 @@ import re, sys, os
 # Validate commits like https://www.conventionalcommits.org/en/v1.0.0/
 
 def main():
-    # example:
-    # feat(apikey): added the ability to add api key to configuration
     pattern = r'(build|ci|docs|feat|fix|perf|refactor|style|test|revert)(\([\w\-]+\))?:\s.*'
     commit = sys.argv[2]
-    m = re.match(pattern, commit)
-    if m is None:
-        print("Conventional commit validation failed")
-        print("  Commit types: build|ci|docs|feat|fix|perf|refactor|style|test|revert")
-        print("Example: feat(parser): add ability to parse arrays.")
-        sys.exit(1)
-    os.system(sys.argv[1] + ' commit -m "' + commit + '"')
+    commit = commit[7:]
+    if commit[0:-2] == '-m':
+        m = re.match(pattern, commit)
+        if m is None:
+            print("Conventional commit validation failed")
+            print("  Commit types: build|ci|docs|feat|fix|perf|refactor|style|test|revert")
+            print("Example: feat(parser): add ability to parse arrays.")
+            sys.exit(1)
+        os.system(sys.argv[1] + ' commit -m "' + commit + '"')
 
 if __name__ == "__main__":
     main()
