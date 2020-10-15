4648ae2bc50e13b17cd326e4b4b221ec66c4be1b
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-15 17:47:14 +0200

    feat(commit): fix commit

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -15,6 +15,6 @@ def main():
             print("Example: feat(parser): add ability to parse arrays.")
             sys.exit(1)
         os.system(sys.argv[1] + ' commit -m "' + commit + '"')
-
+        sys.exit(0)
 if __name__ == "__main__":
     main()
