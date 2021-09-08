c54f54e13a2cff447dd0bfc5d0e30c7d467856a8
Daniele Scasciafratte <mte90net@gmail.com>
2021-09-08 19:51:14 +0200

    feat(fork): improved support

--- hooks/pre-clone.sh
+++ hooks/pre-clone.sh
@@ -8,17 +8,23 @@ set -- $*
 url=$4
 url=${url%/}
 if [[ $3 == "--fork" ]]; then
-    if [[ $url == *"https://github.com/"* ]]; then
+    re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)$"
+    if [[ $url =~ $re ]]; then
+        protocol=${BASH_REMATCH[1]}
+        separator=${BASH_REMATCH[2]}
+        hostname=${BASH_REMATCH[3]}
+        user=${BASH_REMATCH[4]}
+        repo=${BASH_REMATCH[5]}
+
         echo "$url download in progress"
-        git clone "git@github.com:$url.git" &> /dev/null
-        user=$(echo "$url" | awk -F/ '{print $1}')
-        repo=$(echo "$url" | awk -F/ '{print $NF}')
+        /usr/bin/git clone "git@github.com:$user/$repo.git" &> /dev/null
         cd $repo
         remote=$(curl -s "https://api.github.com/repos/$user/$repo" | jq -r '.parent.clone_url' | tail -c +20)
+        echo "https://api.github.com/repos/$user/$repo"
         if [ "$remote" != "" ]; then
             echo "$remote download in progress"
-            git remote add upstream "git@github.com:$remote" &> /dev/null
-            git fetch --all &> /dev/null
+            /usr/bin/git remote add upstream "git@github.com:$remote" &> /dev/null
+            /usr/bin/git fetch --all &> /dev/null
         fi 
         exit 1
     fi
