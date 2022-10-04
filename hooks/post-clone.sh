889504dfd9f8c53925fab8b8c240afafc3ca20c3
stronkness <andrefrisk98@gmail.com>
2022-10-04 21:39:55 +0200

    Add statements for dependency installers
    Added statements for checking if files exists and versions of pip, npm
    and composer is installed. If so, install the dependencies from the
    files

--- hooks/post-clone.sh
+++ hooks/post-clone.sh
@@ -1,11 +1,42 @@
 # Auto cd after cloning
-set -- $*
+# set -- $*
 
-basename=$(basename $3)
-folder=${basename%.*}
-cwd=$(pwd)
+# basename=$(basename $3)
+# folder=${basename%.*}
+# cwd=$(pwd)
 
-if [[ -d "$cwd/$folder" ]]; then
-    cd "$cwd/$folder"
-    $SHELL
+# if [[ -d "$cwd/$folder" ]]; then
+#     cd "$cwd/$folder"
+#     $SHELL
+# fi
+
+# Install dependencies if they exist, first checks if software is installed
+# npm, package.json
+if [[ -f "package.json" ]]; then
+  if which node &> /dev/null
+    then
+        npm install
+    else
+        echo "[REQUIREMENT] Node not installed. Can't install Node dependencies"
+    fi
+fi
+
+# pip, requirements.txt
+if [[ -f "requirements.txt" ]]; then
+  if which pip &> /dev/null
+    then
+        pip install -r requirements.txt
+    else
+        echo "[REQUIREMENT] pip not installed. Can't install python dependencies"
+    fi
+fi
+
+# composer, composer.json
+if [[ -f "composer.json" ]]; then
+  if which composer &> /dev/null
+    then
+        composer.phar install
+    else
+        echo "[REQUIREMENT] composer not installed. Can't install composer dependencies"
+    fi
 fi
