5ec04d55625093cab7eaf4388f54a8c66f990b4b
André Frisk <50335484+Stronkness@users.noreply.github.com>
2022-10-05 10:18:17 +0200

    Add statements for dependency installers (#7)

--- hooks/post-clone.sh
+++ hooks/post-clone.sh
@@ -9,3 +9,34 @@ if [[ -d "$cwd/$folder" ]]; then
     cd "$cwd/$folder"
     $SHELL
 fi
+
+# Install dependencies if they exist, first checks if software is installed
+# npm, package.json
+if [[ -f "package.json" ]]; then
+  if which npm &> /dev/null
+    then
+        npm install
+    else
+        echo "[REQUIREMENT] Node not installed. Can't install Node dependencies"
+    fi
+fi
+
+# pip, requirements.txt
+if [[ -f "requirements.txt" ]]; then
+  if which pip &> /dev/null
+    then
+        pip install -r requirements.txt
+    else
+        echo "[REQUIREMENT] pip not installed. Can't install python dependencies"
+    fi
+fi
+
+# composer, composer.json
+if [[ -f "composer.json" ]]; then
+  if which composer &> /dev/null
+    then
+        composer install
+    else
+        echo "[REQUIREMENT] composer not installed. Can't install composer dependencies"
+    fi
+fi
