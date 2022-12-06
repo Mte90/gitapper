93b94ad7b3eb590adf57e5d39546703ec6207d49
Daniele Scasciafratte <mte90net@gmail.com>
2022-12-06 13:43:48 +0100

    feat(build): updated

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir ./lib
 
 echo "Downloading Forgit"
-wget https://raw.githubusercontent.com/wfxr/forgit/master/forgit.plugin.zsh -O ./lib/forgit.sh
+wget https://raw.githubusercontent.com/wfxr/forgit/master/bin/git-forgit -O ./lib/forgit.sh
 
 echo "Downloading Git Extras"
 wget https://github.com/tj/git-extras/raw/master/bin/git-rename-branch -O ./lib/rename-branch.sh
