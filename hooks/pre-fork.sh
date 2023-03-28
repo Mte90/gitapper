2b9bf32ff55419d0362ffc989a64f159d5bc8161
Daniele Scasciafratte <mte90net@gmail.com>
2023-03-28 11:50:56 +0200

    fix(fork): now works again

--- hooks/pre-fork.sh
+++ hooks/pre-fork.sh
@@ -6,26 +6,26 @@
 #  - jq
 #  - awk
 set -- $*
-parameters=($1)
 
-url=${parameters[2]}
+url=($3)
 url=${url%/}
 re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)$"
+
 if [[ $url =~ $re ]]; then
         user=${BASH_REMATCH[4]}
         repo=${BASH_REMATCH[5]}
         repo=${repo/\.git/}
 
         echo "$url download in progress"
-        $GIT clone "git@github.com:$user/$repo.git" &> /dev/null
+        $1 clone "git@github.com:$user/$repo.git"
         cd $repo
         remote=$(curl -s "https://api.github.com/repos/$user/$repo" | jq -r '.parent.clone_url' | tail -c +20)
         if [ "$remote" != "" ]; then
             echo "$remote download in progress"
-            $GIT remote add upstream "git@github.com:$remote" &> /dev/null
-            $GIT fetch --all &> /dev/null
+            $1 remote add upstream "git@github.com:$remote" &> /dev/null
+            $1 fetch --all &> /dev/null
         fi
-        $(dirname "$0")/hooks/post-clone.sh ${parameters[0]} ${parameters[1]} ${parameters[2]}
+        $(dirname "$0")/post-clone.sh ${parameters[0]} ${parameters[1]} ${parameters[2]}
         $SHELL
         exit 1
 fi
