ff76ca854292db83e606c4b6a4d72196dbea2a98
Daniele Scasciafratte <mte90net@gmail.com>
2023-05-22 18:31:27 +0200

    fix(forgit): checkout command

--- hooks/pre-checkout.sh
+++ hooks/pre-checkout.sh
@@ -33,6 +33,6 @@ set -- "$*"
 parameter=${parameters[2]}
 if [[ $parameter == "" ]]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    "$DIR"/../lib/forgit.sh checkout-branch selector
+    "$DIR"/../lib/forgit.sh checkout_branch
     exit 1
 fi
